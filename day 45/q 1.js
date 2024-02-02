59a66849debc42acd02abe496561150c54757ccc
ayan3004 <ayansangani3004@gmail.com>
2024-02-02 18:09:20 +0530

    01-02-24

---
+++ day 45/q 1.js
@@ -0,0 +1,16 @@
+function loadnav()
+{
+    const nav=
+    `<nav>
+    <a href="">Home</a>
+    <a href="">About</a>
+    <a href="">Products</a>
+    <a href="">Blog</a>
+    <a href="">Contact Us</a>
+</nav>`
+
+document.getElementById("navbar").innerHTML=nav
+
+}
+
+export{loadnav}
